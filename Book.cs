4c815f345e4a2d11be6b6c29500b0171b312bef9
tiagc <tiago.canha@hotmail.com>
2023-12-13 00:06:28 +0100

    class created

---
+++ Book.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryManagement
+{
+    public class Book
+    {
+        public string Title; 
+        public string Author;
+        public int PublishingYear;
+        public bool IsLent;
+
+        public Book(string title, string author, int publishingYear, bool isLent)
+        {
+            Title = title;
+            Author = author;
+            PublishingYear = publishingYear;
+            IsLent = isLent;
+        }
+
+        public string GetAuthor()
+        {
+            return "asdffsda";
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using LibraryManagement;
+
+public class Program
+{
+    public static void Main()
+    {
+        Console.Title = "Library Manager";
+        Console.ForegroundColor = ConsoleColor.Cyan;
+        Console.WriteLine("**********************************");
+        Console.WriteLine("* Welcome to the digital Library *");
+        Console.WriteLine("**********************************");
+
+        var author = Console.ReadLine();
+        var title = Console.ReadLine();
+        var newBook = new Book(author, "asdffsda", 2020, false);
+    }
+}
